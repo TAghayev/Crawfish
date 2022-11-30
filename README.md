91f3db70b0c8dcf8fba5b9b59b1156c17d0db964
drert <54976369+drert@users.noreply.github.com>
2022-11-29 19:16:50 -0500

    transmission construction is done^TM
    can now:
    generate keys
    
    create transmission from keys
    
    deconstruct a transmission on recieving end
    
    signatures are made and verified automagically, but do not cause discard yet, will adjust later as we see fit.

--- README.md
+++ README.md
@@ -6,5 +6,4 @@ It'll make sense later, yes.
 list of libraries needed:
 
 numpy + other standards
-PySock
-socket (maybe base?)
+
--- transmission_construction.py
+++ transmission_construction.py
@@ -1,6 +1,8 @@
 import rsa
 import ecdsa
+import pickle
 
+enc_standard = 'utf8'
 def generate_keys() :
     (pub, priv) = rsa.newkeys(2048)
     sign = ecdsa.SigningKey.generate()
@@ -8,13 +10,13 @@ def generate_keys() :
     return pub, priv, sign, verif
 
 def encrypt_message(public_key, message) :
-    enc_msg = message.encode('utf8')
+    enc_msg = message.encode(enc_standard)
     encrypted_message = rsa.encrypt(enc_msg, public_key)
     return encrypted_message
 
 def decrypt_message(private_key, cryptid) :
     decrypted_message = rsa.decrypt(cryptid, private_key)
-    orig_message = decrypted_message.decode('utf8')
+    orig_message = decrypted_message.decode(enc_standard)
     return orig_message
 
 def sign_message(signing_key, encrypted_message, target_id) :
@@ -30,30 +32,64 @@ def verify_signature(verification_key, encrypted_message, target_id, signature) 
         verified = False
     return verified
 
-def construct_transmission(message, target_id, target_pk, signing_key) :
-    return
+def construct_transmission(message, target_id, target_pubkey, signing_key) :
+    # take in message, target_id, target_pk, and signing_key
+    # encrypt the message, sign it, and send it all in an encoded tuple.
 
-def deconstruct_transmission(transmission, private_key) :
-    return
+    encrypted_message = encrypt_message(target_pubkey, message)
+    signature = sign_message(signing_key, encrypted_message, target_id)
+    transmission = pickle.dumps((target_id, encrypted_message, signature))
+    return transmission
 
+def deconstruct_transmission(transmission, private_key, verification_key) :
+    # take in encoded tuple, rip it apart
+    # verify the message and decrypt the message.
+    target_id, encrypted_message, signature = pickle.loads(transmission)
+    decrypted_message = decrypt_message(private_key, encrypted_message)
+    message_verified = verify_signature(verification_key, encrypted_message, target_id, signature)
+    if not message_verified :
+        print("MALFORMED MESSAGE DETECTED")
+    return decrypted_message
+
+def pull_target_id(transmission) :
+    target_id, encrypted_message, signature = pickle.loads(transmission)
+    return target_id
 
 if __name__ == "__main__" :
     secret_message = "This is a secret!"
     target_id = 8081337
-
-    bob_pub, bob_priv, bob_sign, bob_verif = generate_keys()
-
-    # rsa encryption, decryption
-    secret_encrypted = encrypt_message(bob_pub, secret_message)
-    secret_decrypted = decrypt_message(bob_priv, secret_encrypted)
-
-    # ecdsa signature creation, verification
-    secret_signature = sign_message(bob_sign, secret_encrypted, target_id)
-    secret_verified  = verify_signature(bob_verif, secret_encrypted, target_id, secret_signature)
-
-    # print all results
     print(secret_message)
-    print(secret_encrypted)
-    print(secret_signature)
-    print(secret_verified)
-    print(secret_decrypted)
+
+    #THESE WOULD BE GENERATED INDEPENDANTLY!!! but, im doing this to save time.
+    bob_pub, bob_priv, alice_sign, alice_verif = generate_keys()
+
+    # alice creating a transmission to send
+    transmission = construct_transmission(secret_message, target_id, bob_pub, alice_sign)
+    print(transmission)
+
+    # this is what a hacker is able to decode from the transmission
+    print(pickle.loads(transmission))
+    
+    # server finds the target_id, to determine the message is meant for bob
+    target_id = pull_target_id(transmission)
+    print(target_id)
+
+    # bob after recieving transmission
+    recieved_message = deconstruct_transmission(transmission, bob_priv, alice_verif)
+    print(recieved_message)
+
+
+    # # rsa encryption, decryption
+    # secret_encrypted = encrypt_message(bob_pub, secret_message)
+    # secret_decrypted = decrypt_message(bob_priv, secret_encrypted)
+
+    # # ecdsa signature creation, verification
+    # secret_signature = sign_message(bob_sign, secret_encrypted, target_id)
+    # secret_verified  = verify_signature(bob_verif, secret_encrypted, target_id, secret_signature)
+
+    # # print all results
+    # print(secret_message)
+    # print(secret_encrypted)
+    # print(secret_signature)
+    # print(secret_verified)
+    # print(secret_decrypted)
