65b1706572bfb536b062e82d8bf5572a4f1c314e
drert <54976369+drert@users.noreply.github.com>
2022-12-01 21:53:25 -0500

    cont. of previous
    some implementation started for new client and server, brain hurty so stopping for now
    
    commented client throuroughly explaining what is needed where
    
    CORRECT ARGUMENTS FOR ALL FUNCTIONS NOT IMPLEMENTED!
    ARE PRONE TO CHANGE!

---
+++ client.py
@@ -0,0 +1,64 @@
+import threading
+import socket
+import transmission_construction as tc
+
+# definitions
+enc_standard = 'utf8'
+msg_len = 1028
+host = '127.0.0.1' # Local Host
+port = 55555 # Port
+
+def send_transmission() :
+    return
+
+def process_recieved_transmission() :
+    return
+
+def write_engine(client, pub, verif) :
+    # startup
+    # send keys to server, 
+    client.send()
+
+    while True :
+        return
+
+def read_engine(client) :
+    # startup
+    # recieves current public key array (the server should send this to the
+    # new client first, hence no checks are necessary for )
+
+    # main process engine
+    while True :
+        return 
+        try:
+            inbound_tr = client.recv(msg_len)
+            input = process_recieved_transmission(inbound_tr)
+            if input is None :
+                continue # None occurs on error in deconstructing the transmission (faulty transmission or tampering detected)
+            
+            
+
+        except :
+            print("ERROR")
+            client.close()
+            break
+
+    
+    
+if __name__ == "__main__" :
+    # main function
+
+    # establish connection (standard socket stuff)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    # generate keys before even bothering with anything else
+    pub, priv, sign, verif = tc.generate_keys()
+
+    # write thread
+    wt = threading.Thread(target = write_engine(client, pub, verif))
+    wt.start()
+
+    # read thread
+    rt = threading.Thread(target = read_engine(client))
+    rt.start()
--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import threading 
 import socket
+import transmission_construction as tc
 
+enc_standard = 'utf8'
+msg_len = 1028
 host = '127.0.0.1' # Local Host
 port = 55555 # Port
 
-IDs = [[],[],[]] 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start the server
-server.bind((host, port)) #Server is bound to the local host on our port
-server.listen() #Port starts listening
+
+if __name__ == "__main__" :
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Start the server
+    server.bind((host, port)) #Server is bound to the local host on our port
+    server.listen() #Port starts listening
